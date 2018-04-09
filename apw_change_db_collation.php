9f58b08d5d69c73ac92596341adfdbc1602fffd5
Patrick Weightman <pweightman@apwtradibng.co.uk>
2018-04-09 11:21:47 +0100

    1st full working version
    1st full working version, still needs sql statements to be applied manually

--- apw_change_db_collation.php
+++ apw_change_db_collation.php
@@ -4,6 +4,9 @@
 	$database = "database";
 	$uid = "usedid";
 	$passwd = "password";
+	
+	$collation = "utf8_general_ci";
+	$charset = "utf8";
 
 	$dbc=new MySQLi($host,$uid,$passwd,$database);
 	if($dbc->connect_errno){
@@ -11,6 +14,34 @@
     	exit;
 	}
 
+	// Do Database
+	$sql = "ALTER DATABASE " . $database . " CHARACTER SET " . $charset . " COLLATE " . $collation . ";";
+	echo "<div>" . $sql . "</div>\n\n";
+	$dbc->query("SET foreign_key_checks = 0; " . $sql . " SET foreign_key_checks = 1;");
+	
+	// Do Tables
+	$sql = "SELECT concat ( " .
+		"'ALTER TABLE ', " .
+		"t1.TABLE_SCHEMA, " .
+        "'.', " .
+        "t1.table_name, " .
+        " ' CONVERT TO character set " . $charset . " collate " . $collation . ";'" .
+		") " .
+        "FROM " . 
+    	"information_schema.tables t1 " .  
+		"WHERE " .  
+    	"t1.TABLE_SCHEMA like '" . $database .  "' AND " .  
+    	"t1.TABLE_COLLATION IS NOT NULL AND " . 
+    	"t1.TABLE_COLLATION NOT IN ('" . $collation . "'); ";
+	//echo "<div>" . $sql . "</div>\n\n";
+	$res=$dbc->query($sql) or die($dbc->error);
+	while($queries=$res->fetch_array()){
+		$sql = "SET foreign_key_checks = 0; " . $queries[0] . " SET foreign_key_checks = 1;";
+		$dbc->query($sql);
+		echo "<div>" . $sql . "</div>\n\n";
+	}
+
+	// Do Columns
 	$sql = "SELECT concat ( " .
             "'ALTER TABLE ', " .
                 "t1.TABLE_SCHEMA, " .
@@ -23,19 +54,20 @@
                 "'(' , " .
                     "CHARACTER_MAXIMUM_LENGTH, " .
                 "')', " .
-                "' CHARACTER SET utf8 COLLATE utf8_general_ci;' " .
+                "' CHARACTER SET " . $charset . " COLLATE " . $collation . ";' " .
         ") " .
 		"FROM " .
     	" information_schema.columns t1 " .
 		"WHERE " .
     	" t1.TABLE_SCHEMA like '" . $database .  "' AND " .
     	"t1.COLLATION_NAME IS NOT NULL AND " .
-    	"t1.COLLATION_NAME NOT IN ('utf8_general_ci');";
-    
+    	"t1.COLLATION_NAME NOT IN ('" . $collation . "');";
+ 	//echo "<div>" . $sql . "</div>\n\n";   
 	$res=$dbc->query($sql) or die($dbc->error);
 	while($queries=$res->fetch_array()){
-		$dbc->query($queries[0]);
-		echo "<div>" . $queries[0] . "</div>\n\n";
+		$sql = "SET foreign_key_checks = 0; " . $queries[0] . " SET foreign_key_checks = 1;";
+		$dbc->query($sql);
+		echo "<div>" . $sql . "</div>\n\n";
 	}
 
 ?>
